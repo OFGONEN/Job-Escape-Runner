2c658050fa71dbeedde4218b188fed9dac6214bd
ofgonen <ofgonen@gmail.com>
2021-05-25 16:48:03 +0300

    ColliderListener: Gizmos draws wired cube

--- Assets/Scripts/ColliderListener_EventRaiser.cs
+++ Assets/Scripts/ColliderListener_EventRaiser.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using FFStudio;
+using UnityEditor;
 
 public class ColliderListener_EventRaiser : MonoBehaviour
 {
@@ -14,5 +15,19 @@ public class ColliderListener_EventRaiser : MonoBehaviour
     {
         triggerEnter?.Invoke( other );
     }
+	#endregion
+
+#if UNITY_EDITOR
+	#region EditorOnly 
+	BoxCollider boxCollider;
+	private void OnDrawGizmos()
+    {
+        if( boxCollider == null )
+		    boxCollider = GetComponent< BoxCollider >();
+
+		Handles.color = Color.green;
+		Handles.DrawWireCube( boxCollider.bounds.center, boxCollider.bounds.size );
+	}
     #endregion
+#endif
 }
--- Assets/Scripts/ColliderListener_GameEventRaiser.cs
+++ Assets/Scripts/ColliderListener_GameEventRaiser.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using FFStudio;
+using UnityEditor;
 
 public class ColliderListener_GameEventRaiser : MonoBehaviour
 {
@@ -24,4 +25,18 @@ public class ColliderListener_GameEventRaiser : MonoBehaviour
 
 	#region Implementation
 	#endregion
+
+#if UNITY_EDITOR
+	#region EditorOnly 
+	BoxCollider boxCollider;
+	private void OnDrawGizmos()
+    {
+        if( boxCollider == null )
+		    boxCollider = GetComponent< BoxCollider >();
+
+		Handles.color = Color.green;
+		Handles.DrawWireCube( boxCollider.bounds.center, boxCollider.bounds.size );
+	}
+    #endregion
+#endif
 }
